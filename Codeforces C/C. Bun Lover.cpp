0e0333520df2302d0a2bcfa8e340762636e70921
Miad <154852309+MuhammadMiad@users.noreply.github.com>
2026-06-24 18:36:36 +0600

    Code  with Miad

---
+++ Codeforces C/C. Bun Lover.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+void solve(){
+          ll n;cin>>n;
+          cout<<(n*(n+1))/2+((n+1)*(n+2))/2 +1<<endl;
+
+}
+int main(){
+    int t;cin>>t;while(t--){
+       solve();
+    }
+return 0;
+}
+
