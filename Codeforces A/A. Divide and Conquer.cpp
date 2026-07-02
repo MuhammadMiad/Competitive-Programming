f6f387dbad129cbda1b4d86b394b689fcfc5b778
Miad <154852309+MuhammadMiad@users.noreply.github.com>
2026-07-02 16:59:55 +0600

    Code with Miad

---
+++ Codeforces A/A. Divide and Conquer.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+void solve(){
+            int x,y;cin>>x>>y;
+            if(x<y || x%y!=0)cout<<"NO"<<endl;
+            else cout<<"YES"<<endl;
+
+}
+int main(){
+    int t;cin>>t;while(t--){
+       solve();
+    }
+return 0;
+}
+
